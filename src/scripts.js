fa374132b7699290a904432c9a80f67b5b040d90
Madhav Prahlad <prahladmadhav@gmail.com>
2023-02-24 23:49:10 +0530

    (#002) Add Comments | Replace names of var & id

--- src/scripts.js
+++ src/scripts.js
@@ -1,74 +1,79 @@
-var hourTensVar = 0;
-var hourOnesVar = 0;
+// Time Variables
 var minuteTensVar = 0;
 var minuteOnesVar = 0;
-
+var secondTensVar = 0;
+var secondOnesVar = 0;
+// Interval ID Variable
 var intervalId;
-
-var isStart = false;
-
+// Variable storing true if the timer has started, false if it has not
+var isStarted = false;
+/* 
+    Function allows us to update the status of the timer variables
+    and update the time on the HTML Element(s) corresponding to which
+    the change must occur.
+*/
 var timerFunc = () => {
-  minuteOnesVar++;
-  if (minuteOnesVar == 10) {
-    minuteOnesVar = 0;
-    minuteTensVar++;
-    if (minuteTensVar == 6) {
-      minuteTensVar = 0;
-      hourOnesVar++;
-      if (hourOnesVar == 10) {
-        hourOnesVar = 0;
-        hourTensVar++;
-        if (hourTensVar == 6) {
-          hourTensVar = 0;
+  secondOnesVar++;
+  if (secondOnesVar == 10) {
+    secondOnesVar = 0;
+    secondTensVar++;
+    if (secondTensVar == 6) {
+      secondTensVar = 0;
+      minuteOnesVar++;
+      if (minuteOnesVar == 10) {
+        minuteOnesVar = 0;
+        minuteTensVar++;
+        if (minuteTensVar == 6) {
+          minuteTensVar = 0;
         }
-        document.getElementById("hourTens").innerText = String(hourTensVar);
+        document.getElementById("minuteTens").innerText = String(minuteTensVar);
       }
-      document.getElementById("hourOnes").innerText = String(hourOnesVar);
+      document.getElementById("minuteOnes").innerText = String(minuteOnesVar);
     }
-    document.getElementById("minuteTens").innerText = String(minuteTensVar);
+    document.getElementById("secondTens").innerText = String(secondTensVar);
   }
-  document.getElementById("minuteOnes").innerText = String(minuteOnesVar);
+  document.getElementById("secondOnes").innerText = String(secondOnesVar);
 };
 
 // On the click of Start button
 var startFunc = () => {
-  if (!isStart) {
+  if (!isStarted) {
     intervalId = setInterval(timerFunc, 1000);
-    document.getElementById("startButton").classList.add("inactive")
-    document.getElementById("stopButton").classList.remove("inactive")
-    document.getElementById("resetButton").classList.remove("inactive")
-    isStart = true;
+    document.getElementById("startButton").classList.add("inactive");
+    document.getElementById("stopButton").classList.remove("inactive");
+    document.getElementById("resetButton").classList.remove("inactive");
+    isStarted = true;
   }
 };
 // On the click of Stop button
 var stopFunc = () => {
-  if (isStart) {
+  if (isStarted) {
     clearInterval(intervalId);
+    secondOnesVar = document.getElementById("secondOnes").innerText;
+    secondTensVar = document.getElementById("secondTens").innerText;
     minuteOnesVar = document.getElementById("minuteOnes").innerText;
     minuteTensVar = document.getElementById("minuteTens").innerText;
-    hourOnesVar = document.getElementById("hourOnes").innerText;
-    hourTensVar = document.getElementById("hourTens").innerText;
-    document.getElementById("startButton").classList.remove("inactive")
-    document.getElementById("stopButton").classList.add("inactive")
-    document.getElementById("resetButton").classList.remove("inactive")
-    isStart = false;
+    document.getElementById("startButton").classList.remove("inactive");
+    document.getElementById("stopButton").classList.add("inactive");
+    document.getElementById("resetButton").classList.remove("inactive");
+    isStarted = false;
   }
 };
 // On the click of Reset button
 var resetFunc = () => {
-  if (isStart) {
+  if (isStarted) {
     clearInterval(intervalId);
-    isStart = false;
+    isStarted = false;
   }
+  secondOnesVar = 0;
+  secondTensVar = 0;
   minuteOnesVar = 0;
   minuteTensVar = 0;
-  hourOnesVar = 0;
-  hourTensVar = 0;
+  document.getElementById("secondOnes").innerText = "0";
+  document.getElementById("secondTens").innerText = "0";
   document.getElementById("minuteOnes").innerText = "0";
   document.getElementById("minuteTens").innerText = "0";
-  document.getElementById("hourOnes").innerText = "0";
-  document.getElementById("hourTens").innerText = "0";
-  document.getElementById("startButton").classList.remove("inactive")
-  document.getElementById("stopButton").classList.add("inactive")
-  document.getElementById("resetButton").classList.add("inactive")
+  document.getElementById("startButton").classList.remove("inactive");
+  document.getElementById("stopButton").classList.add("inactive");
+  document.getElementById("resetButton").classList.add("inactive");
 };
